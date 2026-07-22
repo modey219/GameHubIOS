1616eafda2c490aa657413c4bf158728afc44c80
modey219 <shajor149@gmail.com>
2026-07-22 14:29:49 +0300

    DEBUG: ContentView with env objects but just Text - test if env objects crash

--- GameHub/SwiftUI/ContentView.swift
+++ GameHub/SwiftUI/ContentView.swift
@@ -4,14 +4,9 @@ struct ContentView: View {
     @EnvironmentObject var containerManager: ContainerManager
     @EnvironmentObject var jitManager: JITManager
     @EnvironmentObject var settingsManager: SettingsManager
-    @State private var selectedTab = 0
 
     var body: some View {
-        TabView(selection: $selectedTab) {
-            GameLibraryView()
-                .tabItem { Label("Games", systemImage: "gamecontroller") }
-                .tag(0)
-        }
-        .accentColor(.blue)
+        Text("ContentView WORKS")
+            .font(.largeTitle)
     }
 }

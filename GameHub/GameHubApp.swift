3bf84a0cecb7a7928fda0c7ff07eb33304f861b9
modey219 <shajor149@gmail.com>
2026-07-23 08:41:56 +0300

    debug: add crash handler back to isolate

--- GameHub/GameHubApp.swift
+++ GameHub/GameHubApp.swift
@@ -1,8 +1,25 @@
 import SwiftUI
 import UIKit
 
+func setupCrashHandler() {
+    NSSetUncaughtExceptionHandler { exception in
+        let crash = "[Crash] ObjC exception: \(exception.name) reason=\(exception.reason ?? "nil") callStack=\(exception.callStackSymbols.joined(separator: "\n"))"
+        NSLog("%@", crash)
+        if let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
+            let log = path + "/crash.log"
+            try? crash.write(toFile: log, atomically: true, encoding: .utf8)
+        }
+    }
+    if let path = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).first {
+        let crashLogPath = path + "/crash.log"
+        crashLogPath.withCString { install_crash_handler($0) }
+        safeSetenv("CRASH_LOG_PATH", crashLogPath)
+    }
+}
+
 @main
 struct GameHubApp: App {
+    init() { setupCrashHandler() }
     @StateObject private var containerManager = ContainerManager()
     @StateObject private var jitManager = JITManager()
     @StateObject private var settingsManager = SettingsManager()
@@ -32,9 +49,7 @@ struct RootView: View {
             }
         }
         .onAppear {
-            NSLog("[ROOTVIEW] onAppear fired")
             showContent = true
-            NSLog("[ROOTVIEW] showContent set to: \(showContent)")
         }
     }
 }

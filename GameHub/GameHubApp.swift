5760d91dd3e7fce3ce650d9c1a5ab3f768368b4d
modey219 <shajor149@gmail.com>
2026-07-24 06:56:27 +0300

    debug: absolute minimal - just Text, no managers, no RootView

--- GameHub/GameHubApp.swift
+++ GameHub/GameHubApp.swift
@@ -1,64 +1,12 @@
 import SwiftUI
 import UIKit
 
-func swiftLog(_ msg: String) {
-    let ts = ISO8601DateFormatter().string(from: Date())
-    let line = "[\(ts)] \(msg)\n"
-    guard let docs = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
-    let path = docs.appendingPathComponent("app_flow.log").path
-    if let fh = FileHandle(forWritingAtPath: path) {
-        fh.seekToEndOfFile()
-        if let data = line.data(using: .utf8) { fh.write(data) }
-        fh.closeFile()
-    } else {
-        FileManager.default.createFile(atPath: path, contents: line.data(using: .utf8))
-    }
-}
-
 @main
 struct GameHubApp: App {
-    @StateObject private var containerManager = ContainerManager()
-    @StateObject private var jitManager = JITManager()
-    @StateObject private var settingsManager = SettingsManager()
-
     var body: some Scene {
         WindowGroup {
-            RootView(containerManager: containerManager, jitManager: jitManager, settingsManager: settingsManager)
-        }
-    }
-}
-
-struct RootView: View {
-    @ObservedObject var containerManager: ContainerManager
-    @ObservedObject var jitManager: JITManager
-    @ObservedObject var settingsManager: SettingsManager
-    @State private var showContent = false
-
-    var body: some View {
-        ZStack {
-            Color(.systemBackground).ignoresSafeArea()
-
-            if showContent {
-                ContentView()
-                    .environmentObject(containerManager)
-                    .environmentObject(jitManager)
-                    .environmentObject(settingsManager)
-            } else {
-                VStack(spacing: 16) {
-                    Image(systemName: "gamecontroller.fill")
-                        .font(.system(size: 64))
-                        .foregroundStyle(LinearGradient(colors: [.blue, .purple], startPoint: .topLeading, endPoint: .bottomTrailing))
-                    Text("MN emulator").font(.largeTitle).bold()
-                    Text("PC Game Emulator for iPhone & iPad").font(.subheadline).foregroundColor(.secondary)
-                    Text("Created by @R_MOX").font(.caption).foregroundColor(.secondary)
-                    ProgressView().scaleEffect(1.2)
-                }
-                .onAppear { swiftLog("Splash appeared") }
-            }
-        }
-        .onAppear {
-            swiftLog("RootView.onAppear: setting showContent=true")
-            showContent = true
+            Text("HELLO - App works!")
+                .font(.largeTitle)
         }
     }
 }
--- GameHub/SwiftUI/ContentView.swift
+++ GameHub/SwiftUI/ContentView.swift
@@ -4,10 +4,8 @@ struct ContentView: View {
     @EnvironmentObject var containerManager: ContainerManager
     @EnvironmentObject var jitManager: JITManager
     @EnvironmentObject var settingsManager: SettingsManager
-    @State private var selectedTab = 0
 
     var body: some View {
         Text("Hello - ContentView works!")
-            .onAppear { swiftLog("ContentView: Hello appeared!") }
     }
 }

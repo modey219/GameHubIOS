3599c8d7756b12efde050781eccfcae9b4243c49
modey219 <shajor149@gmail.com>
2026-07-24 07:21:39 +0300

    debug: test ContainerManager alone

--- GameHub/GameHubApp.swift
+++ GameHub/GameHubApp.swift
@@ -3,9 +3,11 @@ import UIKit
 
 @main
 struct GameHubApp: App {
+    @StateObject private var containerManager = ContainerManager()
+
     var body: some Scene {
         WindowGroup {
-            Text("HELLO - App works!")
+            Text("ContainerManager OK - \(containerManager.containers.count) containers")
                 .font(.largeTitle)
         }
     }

8926ec0eadd5bb39424c93007afe0ec9b1a123b1
modey219 <shajor149@gmail.com>
2026-07-30 19:56:02 +0300

    revert to single GameLibraryView to fix TabView scene-create watchdog crash

--- GameHub/SwiftUI/ContentView.swift
+++ GameHub/SwiftUI/ContentView.swift
@@ -5,34 +5,8 @@ struct ContentView: View {
     @EnvironmentObject var jitManager: JITManager
     @EnvironmentObject var settingsManager: SettingsManager
     @EnvironmentObject var setupManager: SetupManager
-    @State private var selectedTab = 0
 
     var body: some View {
-        mainTabs
-    }
-
-    private var mainTabs: some View {
-        TabView(selection: $selectedTab) {
-            GameLibraryView()
-                .tabItem { Label("Games", systemImage: "gamecontroller") }
-                .tag(0)
-
-            ContainerListView()
-                .tabItem { Label("Containers", systemImage: "shippingbox") }
-                .tag(1)
-
-            SettingsView()
-                .tabItem { Label("Settings", systemImage: "gear") }
-                .tag(2)
-
-            JITStatusView()
-                .tabItem { Label("JIT", systemImage: "bolt.fill") }
-                .tag(3)
-
-            DebugView()
-                .tabItem { Label("Debug", systemImage: "ladybug") }
-                .tag(4)
-        }
-        .accentColor(.blue)
+        GameLibraryView()
     }
 }

94b078692b50723c4b86db374d133bba1e8f47ac
modey219 <shajor149@gmail.com>
2026-07-30 14:58:44 +0300

    stage 10e: completely flatten GameLibraryView to bare Text to isolate the trigger

--- GameHub/SwiftUI/ContentView.swift
+++ GameHub/SwiftUI/ContentView.swift
@@ -1,8 +1,5 @@
 import SwiftUI
 
-// DIAGNOSTIC stage 10d: testing if GameLibraryView.swift as a FILE
-// (even with a trivial body) causes the crash. Creating a minimal
-// inline child view instead.
 struct ContentView: View {
     @EnvironmentObject var containerManager: ContainerManager
     @EnvironmentObject var jitManager: JITManager
@@ -10,12 +7,6 @@ struct ContentView: View {
     @EnvironmentObject var setupManager: SetupManager
 
     var body: some View {
-        InlineGameView()
-    }
-}
-
-struct InlineGameView: View {
-    var body: some View {
-        Text("Hello from inline")
+        GameLibraryView()
     }
 }

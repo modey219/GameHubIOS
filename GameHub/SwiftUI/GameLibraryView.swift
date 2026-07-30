81162891fa7fa1aa7c03ef88198ff63c5f58c48f
modey219 <shajor149@gmail.com>
2026-07-30 15:55:32 +0300

    stage 11c: test @State without selectedGame (Container.Container?)

--- GameHub/SwiftUI/GameLibraryView.swift
+++ GameHub/SwiftUI/GameLibraryView.swift
@@ -1,12 +1,10 @@
 import SwiftUI
 
-// DIAGNOSTIC stage 11b: add all @State properties
 struct GameLibraryView: View {
     @State private var searchText = ""
     @State private var showSearch = false
     @State private var showAddGame = false
     @State private var showImportSheet = false
-    @State private var selectedGame: ContainerManager.Container?
 
     var body: some View {
         Text("hello")
